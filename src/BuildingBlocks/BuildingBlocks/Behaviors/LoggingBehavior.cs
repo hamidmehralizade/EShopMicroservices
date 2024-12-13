9ae53f8ea58f7ef5d0ec9eefd277de260b13f5ac
hamid <hamid@HAMID>
2024-12-13 20:34:57 +0330

    Add Logging and Validation Behavior Custom Exception Handler Refactor Logger from Features

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace BuildingBlocks.Behaviors;
+
+public class LoggingBehavior<TRequest, TResponse>
+    (ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull, IRequest<TResponse>
+    where TResponse : notnull
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("[START] Handle request={Request} - Response={Response} - RequestData={RequestData}",
+            typeof(TRequest).Name, typeof(TResponse).Name, request);
+
+        var timer = new Stopwatch();
+        timer.Start();
+
+        var response = await next();
+
+        timer.Stop();
+        var timeToken = timer.Elapsed;
+        if (timeToken.Seconds > 3)
+            logger.LogWarning("[PERFORMANCE] The request {Request} took {TimeToken} seconds",
+                typeof(TRequest), timeToken.Seconds);
+
+        logger.LogInformation("[END] Handled {Request} with {Response}", typeof(TRequest), typeof(TResponse).Name);
+        return response;
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,19 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
+
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
 builder.Services.AddMarten(opt =>
 {
     opt.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipline.
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
 
 app.Run();
