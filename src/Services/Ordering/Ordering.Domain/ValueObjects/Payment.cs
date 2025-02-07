5157941ea2426e8f0c01a5ce48c0340909d86f8e
hamid <hamid@HAMID>
2025-02-07 23:55:33 +0330

    Add Domains,Value Objects, Events For Order Aggregate

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,34 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record Payment
+{
+    public string? CardName { get; } = default!;
+    public string CardNumber { get; } = default!;
+    public string Expiration { get; } = default!;
+    public string CVV { get; } = default!;
+    public int PaymentMethod { get; } = default!;
+
+    protected Payment()
+    {
+
+    }
+
+    private Payment(string cardName, string cardNumber, string expiration, string ccv, int paymentMethod)
+    {
+        CardName = cardName;
+        CardNumber = cardNumber;
+        Expiration = expiration;
+        CVV = ccv;
+        PaymentMethod = paymentMethod;
+    }
+
+    public static Payment Of(string cardName, string cardNumber, string expiration, string ccv, int paymentMethod)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+        ArgumentException.ThrowIfNullOrWhiteSpace(ccv);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(ccv.Length, 3);
+
+        return new Payment(cardName, cardNumber, expiration, ccv, paymentMethod);
+    }
+}
